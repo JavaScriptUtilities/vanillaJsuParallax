1e208f072493ad4ffdcc5f8b54c6d82ff5ae764b
Kevin <kevin@rocher-dev.com>
2016-12-26 11:34:20 +0100

    v 0.3.0
    * No longer trying to add a property on a read-only object.
    * Add editor config.
    * Better uglification :  < 1ko.

--- js/vanilla-jsuparallax.js
+++ js/vanilla-jsuparallax.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JSU Parallax
- * Version: 0.2.1
+ * Version: 0.3.0
  * Plugin URL: https://github.com/JavaScriptUtilities/vanillaJsuParallax
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -9,75 +9,84 @@ var vanillaJsuParallax = function(settings) {
     'use strict';
 
     var self = this,
-        winHeight = 0;
+        itemCallback,
+        itemPrepare,
+        itemPerspective = 0,
+        winHeight = 0,
+        items = [],
+        itemsOpts = [],
+        itemsLen = 0;
 
-    self.items = [];
-    self.defaultSettings = {};
-
-    self.init = function(settings) {
+    var initFun = function(settings) {
         /* Opts */
-        self.items = settings.items || [];
-        self.itemsLen = self.items.length;
-        self.itemCallback = settings.itemCallback || self.itemCallbackDefault;
-        self.itemPrepare = settings.itemPrepare || self.itemPrepareDefault;
-        self.perspective = settings.perspective || 200;
+        items = settings.items || [];
+        itemsLen = items.length;
+        itemCallback = settings.itemCallback || itemCallbackDefault;
+        itemPrepare = settings.itemPrepare || itemPrepareDefault;
+        itemPerspective = settings.perspective || 200;
 
         /* Prepare items */
-        for (var i = 0; i < self.itemsLen; i++) {
-            self.items[i] = self.itemPrepareDefault(self.items[i]);
+        for (var i = 0; i < itemsLen; i++) {
+            itemPrepare(i, items[i]);
         }
 
         /* Monitor position on scroll, resize, and initial state */
-        window.addEventListener('resize', self.scrollEvent, 1);
-        document.addEventListener('scroll', self.scrollEvent, 1);
-        self.scrollEvent();
+        window.addEventListener('resize', scrollEvent, 1);
+        document.addEventListener('scroll', scrollEvent, 1);
+        scrollEvent();
     };
 
     /* Scroll Event */
-    self.scrollEvent = function() {
+    var scrollEvent = function() {
         winHeight = parseInt(window.innerHeight, 10);
-        for (var i = 0; i < self.itemsLen; i++) {
-            self.setScrollItem(self.items[i]);
+        for (var i = 0; i < itemsLen; i++) {
+            setScrollItem(i, items[i]);
         }
     };
 
-    self.setScrollItem = function(item) {
+    var setScrollItem = function(i, item) {
         var rect = item.getBoundingClientRect(),
             elCenter = (rect.top + rect.bottom) / 2,
             distMiddle = elCenter / winHeight - 1 / 2;
-        self.itemCallback(item, distMiddle);
+        itemCallback(i, item, distMiddle);
     };
 
     /* Default callbacks */
-    self.itemPrepareDefault = function(item) {
+    var itemPrepareDefault = function(i, item) {
         var opt = {};
         /* Movement direction */
         opt.dir = item.getAttribute('data-jsuplxdir') == 'bottom' ? 'bottom' : 'top';
         /* Movement perspective */
-        var itemPerspective = parseInt(item.getAttribute('data-jsuplxperspective'), 10);
-        opt.perspective = itemPerspective ? itemPerspective : self.perspective;
+        var elPerspective = parseInt(item.getAttribute('data-jsuplxperspective'), 10);
+        opt.perspective = elPerspective ? elPerspective : itemPerspective;
         /* Save options */
-        item.jsuParallaxOpts = opt;
+        itemsOpts[i] = opt;
         /* Initial style */
-        item.firstElementChild.style.willChange = 'transform';
-        return item;
+        if (item.firstElementChild) {
+            item.firstElementChild.style.willChange = 'transform';
+        }
+        return true;
     };
 
-    self.itemCallbackDefault = function(item, distMiddle) {
-        var dist = distMiddle * item.jsuParallaxOpts.perspective;
-        if (item.jsuParallaxOpts.dir == 'bottom') {
+    var itemCallbackDefault = function(i, item, distMiddle) {
+        var dist = distMiddle * itemsOpts[i].perspective;
+        if (itemsOpts[i].dir == 'bottom') {
             dist = 0 - dist;
         }
-        self.setTransform(item.firstElementChild, 0, dist, 0);
+        setTransformTranslate3d(item.firstElementChild, 0, dist, 0);
     };
 
     /* Helper */
-    self.setTransform = function(item, x, y, z) {
-        item.style.webkitTransform = 'translate3d(' + x + 'px,' + y + 'px,' + z + 'px)';
-        item.style.mozTransform = 'translate3d(' + x + 'px,' + y + 'px,' + z + 'px)';
-        item.style.transform = 'translate3d(' + x + 'px,' + y + 'px,' + z + 'px)';
+    var setTransformTranslate3d = function(item, x, y, z) {
+        itemStyleSetTransform(item, 'translate3d(' + x + 'px,' + y + 'px,' + z + 'px)');
     };
 
-    self.init(settings);
+    var itemStyleSetTransform = function(item, transformString) {
+        item.style.webkitTransform = transformString;
+        item.style.mozTransform = transformString;
+        item.style.transform = transformString;
+    };
+
+    initFun(settings);
     return self;
 };
